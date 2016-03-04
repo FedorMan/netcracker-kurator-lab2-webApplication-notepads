d4f95a95dfd71b98d7adb0abe6e37846ebaa1443
FedorMan <fedor.leontev@gmail.com>
2016-03-04 18:19:21 +0400

    исправлена передача данных json файлом

--- src/main/java/ru/bk/leontev/fedor/webController/WebController.java
+++ src/main/java/ru/bk/leontev/fedor/webController/WebController.java
@@ -1,14 +1,14 @@
 package ru.bk.leontev.fedor.webController;
 
-import ru.bk.leontev.fedor.controller.Controller;
-import ru.bk.leontev.fedor.models.Entity;
-import ru.bk.leontev.fedor.models.User;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.LinkedList;
+import com.google.gson.Gson;
+import ru.bk.leontev.fedor.controller.Controller;
+import ru.bk.leontev.fedor.models.Entity;
+import ru.bk.leontev.fedor.models.User;
 
 @Path(value="/w")
 public class WebController {
@@ -16,19 +16,23 @@ public class WebController {
     @GET
     @Path(value="/getall")
     @Produces(MediaType.APPLICATION_JSON)
-    public LinkedList<User> listAllUsers() {
+    public String listAllUsers() {
+        Gson gson = new Gson();
         LinkedList<User> message=new LinkedList<User>();
         LinkedList<Entity> entites=controller.listOnQuery("select * from users", User.class);
         for (int i = 0; i <entites.size(); i++) {
             message.add((User)entites.get(i));
         }
-        return message;
+        String json = gson.toJson(message);
+        return json;
     }
     @GET
     @Path(value="/get")
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-
-        return "hello";
+        Gson gson = new Gson();
+        User user=new User("fbdb","fdb");
+        String json = gson.toJson(user);
+        return json;
     }
 }
