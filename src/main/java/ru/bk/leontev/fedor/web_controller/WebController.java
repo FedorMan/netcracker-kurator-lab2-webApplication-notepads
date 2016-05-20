bc9a46b2b6a307fa3167bc8193e16351eb498887
FedorMan <fedor.leontev@gmail.com>
2016-05-20 11:34:24 +0400

    Подгрузка записей по нажатию на блокнот

--- src/main/java/ru/bk/leontev/fedor/web_controller/WebController.java
+++ src/main/java/ru/bk/leontev/fedor/web_controller/WebController.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URI;
 import java.util.LinkedList;
+
 import com.google.gson.Gson;
 import ru.bk.leontev.fedor.controller.Controller;
 import ru.bk.leontev.fedor.models.Entity;
@@ -18,32 +19,10 @@ import ru.bk.leontev.fedor.models.Notepad;
 import ru.bk.leontev.fedor.models.Record;
 import ru.bk.leontev.fedor.models.User;
 
-@Path(value="/w")
+@Path(value = "/w")
 public class WebController {
-    Controller controller=new Controller();
+    Controller controller = new Controller();
     Gson gson = new Gson();
-
-    @GET
-    @Path(value="/getNotepads")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getNotepades() {
-        HttpSession session = request.getSession(false);
-        System.out.println(session.getAttribute("userId"));
-        LinkedList<Entity> notepads=controller.listOnQuery("select * from notepads where id_user="+(Integer)(session.getAttribute("userId")), Notepad.class);
-        String json = gson.toJson(notepads);
-        return json;
-    }
-
-    @POST
-    @Path(value="/saveNotepad")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void saveNotepad(String content) {
-        System.out.println("Save Notepad");
-        Notepad notepad=gson.fromJson(content, Notepad.class);
-        HttpSession session = request.getSession(false);
-        notepad.setId_user((Integer)(session.getAttribute("userId")));
-        controller.save(notepad);
-    }
 
     @Context
     private HttpServletRequest request;
@@ -51,20 +30,53 @@ public class WebController {
     @Context
     private HttpServletResponse response;
 
+
+
+
+
+
+
+
+
+
+
+    @GET
+    @Path(value = "/getNotepads")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getNotepades() {
+        HttpSession session = request.getSession(false);
+        System.out.println(session.getAttribute("userId"));
+        LinkedList<Entity> notepads = controller.listOnQuery("select * from notepads where id_user=" + (Integer) (session.getAttribute("userId")), Notepad.class);
+        String json = gson.toJson(notepads);
+        return json;
+    }
+
     @POST
-    @Path(value="/login")
-    public Response getUser(@FormParam("username") String username,@FormParam("username") String password) throws ServletException, IOException {
-        LinkedList<Entity> entites=controller.listOnQuery("select * from users where login="+"'"+username+"'"+" and password="+"'"+password+"'", User.class);
-        if (entites.size()==1){
+    @Path(value = "/saveNotepad")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void saveNotepad(String content) {
+        System.out.println("Save Notepad");
+        Notepad notepad = gson.fromJson(content, Notepad.class);
+        HttpSession session = request.getSession(false);
+        notepad.setId_user((Integer) (session.getAttribute("userId")));
+        controller.save(notepad);
+    }
+
+    @POST
+    @Path(value = "/login")
+    public Response getUser(@FormParam("username") String username, @FormParam("password") String password) throws ServletException, IOException {
+        LinkedList<Entity> entites = controller.listOnQuery("select * from users where login=" + "'" + username + "'" + " and password=" + "'" + password + "'", User.class);
+        if (entites.size() == 1) {
             HttpSession session = request.getSession(true);
             session.setAttribute("userId", ((User) entites.get(0)).getId());
+            session.setAttribute("username", ((User) entites.get(0)).getLogin());
             response.sendRedirect("/index.html");
         }
         return null;
     }
 
     @GET
-    @Path(value="/dropSession")
+    @Path(value = "/dropSession")
     @Produces(MediaType.APPLICATION_JSON)
     public void dropSession() {
         System.out.println("dropSession");
@@ -74,12 +86,22 @@ public class WebController {
     }
 
     @GET
-    @Path(value="/loadRecords")
+    @Path(value = "/loadRecords")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getRecordings(String idNotepad) {
-        System.out.println("loadRecords");
-        LinkedList<Entity> recordings=controller.listOnQuery("select * from recording where id_notepad="+Integer.parseInt(idNotepad), Record.class);
+    public String loadRecords(@QueryParam("idNotepad")String idNotepad) {
+        //System.out.println("loadRecords from: " + idNotepad);
+        LinkedList<Entity> recordings = controller.listOnQuery("select * from recording where id_notepad=" + Integer.parseInt(idNotepad), Record.class);
         String json = gson.toJson(recordings);
+        return json;
+    }
+
+    @GET
+    @Path(value = "/getUsername")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUsername() {
+        HttpSession session = request.getSession(false);
+        String json = gson.toJson(session.getAttribute("username"));
+        //System.out.println(session.getAttribute("username"));
         return json;
     }
 }
